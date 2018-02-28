65f1fd582918e1e9c03aef1b279b1d75725fc3a8
Arash Affra Bakhtiari <arashaffra.bakhtiari@gmail.com>
2018-02-28 16:27:17 +0100

    Blur Commit

---
+++ Prototyp/Blur.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Prototyp
+{
+    class Blur
+    {
+        //copypasted code, prob wont work
+        private static Bitmap Blurs(Bitmap image, Rectangle rectangle, Int32 blurSize)
+        {
+            Bitmap blurred = new Bitmap(image.Width, image.Height);
+
+            // make an exact copy of the bitmap provided
+            using (Graphics graphics = Graphics.FromImage(blurred))
+                graphics.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height),
+                    new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
+
+            // look at every pixel in the blur rectangle
+            for (Int32 xx = rectangle.X; xx < rectangle.X + rectangle.Width; xx++)
+            {
+                for (Int32 yy = rectangle.Y; yy < rectangle.Y + rectangle.Height; yy++)
+                {
+                    Int32 avgR = 0, avgG = 0, avgB = 0;
+                    Int32 blurPixelCount = 0;
+
+                    // average the color of the red, green and blue for each pixel in the
+                    // blur size while making sure you don't go outside the image bounds
+                    for (Int32 x = xx; (x < xx + blurSize && x < image.Width); x++)
+                    {
+                        for (Int32 y = yy; (y < yy + blurSize && y < image.Height); y++)
+                        {
+                            Color pixel = blurred.GetPixel(x, y);
+
+                            avgR += pixel.R;
+                            avgG += pixel.G;
+                            avgB += pixel.B;
+
+                            blurPixelCount++;
+                        }
+                    }
+
+                    avgR = avgR / blurPixelCount;
+                    avgG = avgG / blurPixelCount;
+                    avgB = avgB / blurPixelCount;
+
+                    // now that we know the average for the blur size, set each pixel to that color
+                    for (Int32 x = xx; x < xx + blurSize && x < image.Width && x < rectangle.Width; x++)
+                        for (Int32 y = yy; y < yy + blurSize && y < image.Height && y < rectangle.Height; y++)
+                            blurred.SetPixel(x, y, Color.FromArgb(avgR, avgG, avgB));
+                }
+            }
+
+            return blurred;
+        }
+    }
+}
---
+++ Prototyp/Levels/Level1Blur.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Prototyp
+{
+
+    //DELETE
+    //public sealed class BlurBitmapEffect
+        
+
+    class Level1Blur : Level
+    {
+        private List<Entity> entitys = new List<Entity>();
+        private Entity table1;//rename to avoid bugs
+        private Entity table2;//rename to avoid bugs
+        private Entity drink1;//rename to avoid bugs
+        private float mouseX;
+        private float mouseY;
+
+        
+
+        private State state = State.levelGoing;
+
+        private bool drink1PicktUp = false;//rename to avoid bugs
+
+        public override void CreateLevel()
+        {
+
+            table1 = new Table(new SolidBrush(Color.Chocolate), 800, 400, 90);
+            table2 = new Table(new SolidBrush(Color.Chocolate),100, 400, 90 );
+            drink1 = new Drink(new SolidBrush(Color.Yellow), 120, 420, 20);
+        }
+
+        public override void SetMouseCord(float x, float y)
+        {
+            mouseX = x;
+            mouseY = y;
+
+            if (drink1PicktUp == true)
+            {
+                drink1.Move(mouseX, mouseY);
+            }
+        }
+        public override State Update()
+        {
+           //create eventual blur here
+           //also cheack if items are on tables and if not drop them on the floor
+            return state;
+        }
+
+        public override void PickUpDrink()
+        {
+            RectangleF mouse = new RectangleF(mouseX, mouseY, 1, 1);
+            if (mouse.IntersectsWith(drink1.GetRecF()))
+            {
+                drink1PicktUp = true;
+            }
+        }
+        public override void PlaceDrink()
+        {
+            if (table1.GetRecF().IntersectsWith(drink1.GetRecF()))
+            {
+                drink1PicktUp = false;
+                //state = State.levelDone;
+            }
+            else
+            {
+                drink1PicktUp = false;
+                //drink1.resetEntity();
+            }
+        }
+
+        public override List<Entity> GetEntityList()
+        {
+            return entitys;
+        }
+
+        public override Entity GetDrink()
+        {
+            return drink1;
+        }
+        public override Entity GetTable()
+        {
+            return table1;
+        }
+
+        public override Entity GetTable2()
+        {
+            return table2;
+        }
+    }
+}
