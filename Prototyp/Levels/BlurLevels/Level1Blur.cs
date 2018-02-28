133f10cea9d5096f6a045916b07f2a95d165ce77
Arash Affra Bakhtiari <arashaffra.bakhtiari@gmail.com>
2018-02-28 16:30:52 +0100

    commit2

--- Prototyp/Levels/BlurLevels/Level1Blur.cs
+++ Prototyp/Levels/BlurLevels/Level1Blur.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 
-namespace Prototyp
+/*namespace Prototyp
 {
 
     //DELETE
@@ -94,3 +94,4 @@ namespace Prototyp
         }
     }
 }
+*/
